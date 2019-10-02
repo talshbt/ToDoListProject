6086b4dd599f1d65a250141966a0feced7587a5c
Tal Shabtai <talshbt@gmail.com>
2019-10-02 10:54:26 +0300

    update

---
+++ src/ToDoUIController.java
@@ -0,0 +1,93 @@
+
+
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class ToDoUIController {
+
+    private String read(InputStream is) throws IOException {
+        BufferedReader in = null;
+        String inputLine;
+        StringBuilder body;
+        try {
+            in = new BufferedReader(new InputStreamReader(is));
+
+            body = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                body.append(inputLine);
+            }
+            in.close();
+
+            return body.toString();
+        } catch(IOException ioe) {
+            throw ioe;
+        } finally {
+            this.closeQuietly(in);
+        }
+    }
+    
+
+    protected void closeQuietly(Closeable closeable) {
+        try {
+            if( closeable != null ) {
+                closeable.close();
+            }
+        } catch(IOException ex) {
+
+        }
+    }
+    
+    public String post(String postUrl, String data) throws IOException {
+        URL url = new URL(postUrl);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+
+        con.setDoOutput(true);
+
+        this.sendData(con, data);
+
+        return this.read(con.getInputStream());
+    }
+    
+    protected void sendData(HttpURLConnection con, String data) throws IOException {
+        DataOutputStream wr = null;
+        try {
+            wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(data);
+            wr.flush();
+            wr.close();
+        } catch(IOException exception) {
+            throw exception;
+        } finally {
+            this.closeQuietly(wr);
+        }
+    }
+    
+    
+    
+	public static void main(String[] args) throws IOException {
+       
+		
+		String serverUrl = "http://localhost:5555/addNote";
+		try {
+			ToDoUIController hce = new ToDoUIController();
+            String body = hce.post(serverUrl, "NoteMessage=test data");
+            System.out.println(body);
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+		
+		
+		
+	}
+
+}
